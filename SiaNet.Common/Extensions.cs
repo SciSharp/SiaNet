21f1afe4cfc718e7673265cf9e4d799d76031eaf
Deepak Kumar <deepak_kumarb@live.com>
2018-11-23 11:29:10 +1030

    Refactoring and fixed project references

---
+++ SiaNet.Common/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiaNet.Common
+{
+    /// <summary>
+    /// Extention class for various extension methods for generic list
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Means the specified values.
+        /// </summary>
+        /// <param name="values">The values.</param>
+        /// <returns>System.Double.</returns>
+        public static double Mean(this List<double> values)
+        {
+            return values.Average();
+        }
+
+        /// <summary>
+        /// Standards the specified values.
+        /// </summary>
+        /// <param name="values">The values.</param>
+        /// <returns>System.Double.</returns>
+        public static double Std(this List<double> values)
+        {
+            double average = values.Average();
+            double sumOfDerivation = 0;
+            foreach (double value in values)
+            {
+                sumOfDerivation += (value) * (value);
+            }
+
+            double sumOfDerivationAverage = sumOfDerivation / (values.Count - 1);
+            return Math.Sqrt(sumOfDerivationAverage - (average * average));
+        }
+    }
+}
